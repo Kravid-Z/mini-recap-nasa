e06edb7206439b2a4614007428c031a571e38da7
Ubeyt Demir <190541075@firat.edu.tr>
2021-03-11 09:24:27 +0300

    adds mini recap nasa files

---
+++ js/nasa.main.js
@@ -0,0 +1,15 @@
+
+const searchButtonHandler = function(){
+    const searchInput = document.getElementById("search-input")
+    const query = searchInput.value
+    if(query){
+        window.location.replace(`/search.html?query=${searchInput.value}`)
+    }
+    else{
+        alert('Please type something to search 🤓')
+    }
+}
+window.onload=function(){
+    const searchButton = document.getElementById("search-button")
+    searchButton.onclick=searchButtonHandler
+}
